226e16bba19b178aef9e028e15478b2703a4ff2d
Manjula-Nagarajan <manjulanagarajan1053@gmail.com>
2025-01-12 16:08:58 +0530

    23

--- Backend/Signup/Signup/src/main/java/com/example/Signup/Entity/Users.java
+++ Backend/Signup/Signup/src/main/java/com/example/Signup/Entity/Users.java
@@ -3,37 +3,42 @@ package com.example.Signup.Entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.util.Date;
+import java.util.List;
 
 
 @Entity
 @Data
-@Table(name="userdetails")
+@Table(name="usertable")
 public class Users {
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_id")
     private Integer id;
-    @Column(name="user_name")
 
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
-    @Column(name="user_email")
+
+    @Column(name = "email")
     private String email;
 
-    @Column(name="password")
+    @Column(name = "password")
     private String password;
 
-    @Column(name="mobile")
+    @Column(name = "mobile")
     private Long mobile;
 
-    @Column(name="user_address")
+    @Column(name = "address")
     private String address;
 
-    @Column(name="user_mobile")
-    private String mobile;
+    @CreationTimestamp
+    @Column(name = "registration_date", nullable = false, updatable = false)
+    private Date registrationDate; // This field will store the registration date and time
 
+    @OneToMany
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private List<Feedback> feedbacks;
 }
+
+
--- Backend/Signup/Signup/src/main/java/com/example/Signup/service/AdminService.java
+++ Backend/Signup/Signup/src/main/java/com/example/Signup/service/AdminService.java
@@ -1,9 +1,6 @@
 package com.example.Signup.service;
 
-import com.example.Signup.Entity.Admin;
-import com.example.Signup.Entity.Feedback;
 import com.example.Signup.repository.AdminRepo;
-import com.example.Signup.repository.FeedbackRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
