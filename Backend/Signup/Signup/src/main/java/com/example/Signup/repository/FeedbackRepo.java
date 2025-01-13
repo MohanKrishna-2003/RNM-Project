d29ebc3affd6cb265b41b362b8fbe2f62b35a59b
NayanKishore <nayanadece@gmail.com>
2025-01-13 20:42:02 +0530

    feedback

--- Backend/Signup/Signup/src/main/java/com/example/Signup/repository/FeedbackRepo.java
+++ Backend/Signup/Signup/src/main/java/com/example/Signup/repository/FeedbackRepo.java
@@ -1,19 +1,9 @@
 package com.example.Signup.repository;
 
 import com.example.Signup.Entity.Feedback;
-import com.example.Signup.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public interface FeedbackRepo extends JpaRepository<Feedback, Integer> {
-    // Query to fetch feedbacks with their month and year
-    @Query("SELECT f.feedback_date, f.feedback FROM Feedback f ")
-    List<Object[]> findFeedbacks(); 
+public interface FeedBackRepo extends JpaRepository<Feedback,Integer> {
 }
-
-
